f86551ebb4b25933aa6e36200d9bccd18ef8fff7
Adam B. <adambrbs@gmail.com>
2024-02-14 21:39:18 +1100

    Adds comments to existing functions

--- rockps.js
+++ rockps.js
@@ -4,7 +4,10 @@ const tools = ['rock', 'paper', 'scissors'];
 function getComputerChoice(){
     return tools[(Math.floor(Math.random() * tools.length))];
 }
-
+    // console.log(getComputerChoice());
+    
 function getUserChoice(){
     prompt('Please enter your choice');
 }
+
+   // getUserChoice();
