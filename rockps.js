19f13207ddd78696a2081a61a90d521dd4f5bb57
Adam B. <adambrbs@gmail.com>
2024-02-15 20:58:01 +1100

    Deletes function prompting user for input

--- rockps.js
+++ rockps.js
@@ -4,10 +4,16 @@ const tools = ['rock', 'paper', 'scissors'];
 function getComputerChoice(){
     return tools[(Math.floor(Math.random() * tools.length))];
 }
-    // console.log(getComputerChoice());
-    
-function getUserChoice(){
-    prompt('Please enter your choice');
+    console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == computerSelection){
+        console.log('It\'s a tie');
+    } else {
+        console.log('Try again.');
+    }
 }
 
-   // getUserChoice();
+    const playerSelection = getUserChoice();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
